fc46b4d6d67cd7499e9ca1fee32a08a5bd0fa589
likai <kai.li@elestyle.jp>
2025-04-10 18:16:30 +0900

    fix:compatible

--- src/webpack.ts
+++ src/webpack.ts
@@ -1,6 +1,19 @@
 import { versionInjectorPlugin } from './core';
 import type { VersionInjectorOptions } from './types';
 
-export default function versionInjector(options: VersionInjectorOptions = {}) {
+// 真正的插件工厂函数
+function versionInjector(options: VersionInjectorOptions = {}) {
   return versionInjectorPlugin.webpack(options);
+}
+
+// ✅ 同时导出 ESModule 和 CommonJS 格式
+export default versionInjector;
+
+// ✅ 为 require() 提供兼容：自动注入到 module.exports
+// @ts-ignore
+if (typeof module !== 'undefined') {
+  // @ts-ignore
+  module.exports = versionInjector;
+  // @ts-ignore
+  module.exports.default = versionInjector;
 }
